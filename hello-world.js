d6b452bd62d80fc1cfbb1a60738bea849ef0c2a7
Fide Omar Islas Duarte <fislas@grupoabraxas.com>
2022-06-29 09:17:31 -0500

    Configure space-before-blocks rule in eslint config file

--- hello-world.js
+++ hello-world.js
@@ -2,6 +2,8 @@ const greeting = 'Hello World!';
 
 helloWorld(greeting);
 
-function helloWorld(greeting) {
-  console.log(greeting);
+function helloWorld(greeting){
+  if(greeting === 'Hi'){
+    console.log('Hi World!')
+  } else console.log(greeting);
 }
