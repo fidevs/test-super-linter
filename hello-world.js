2f89c5862c9e7981400b15ec838c7813685a8e03
Fide Omar Islas Duarte <fislas@grupoabraxas.com>
2022-06-29 09:22:02 -0500

    Fix space-before-blocks and semi ESLint rule

--- hello-world.js
+++ hello-world.js
@@ -2,8 +2,8 @@ const greeting = 'Hello World!';
 
 helloWorld(greeting);
 
-function helloWorld(greeting){
-  if(greeting === 'Hi'){
-    console.log('Hi World!')
+function helloWorld(greeting) {
+  if(greeting === 'Hi') {
+    console.log('Hi World!');
   } else console.log(greeting);
 }
