d21ccb46909118ece841fd6143abf43e29b253a7
Fide Omar Islas Duarte <fislas@grupoabraxas.com>
2022-06-29 12:54:10 -0500

    Fix spaced-comment lint issue

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,12 @@
-//Greeting to print in console
+// Greeting to print in console
 const greeting = 'Hello World!';
 
 // Function to execute prints
 helloWorld(greeting);
 
 /**
- *Print some greeting in console
- *@param {string} greeting Greeting to display to user
+ * Print some greeting in console
+ * @param {string} greeting Greeting to display to user
  */
 function helloWorld(greeting) {
   if(greeting === 'Hi') {
