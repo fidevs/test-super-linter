64734613e6d4c9c3bf322176d58adabbb1625888
Fide Omar Islas Duarte <fislas@grupoabraxas.com>
2022-06-30 16:46:10 -0500

    Fix quotes issue

--- hello-world.js
+++ hello-world.js
@@ -1,11 +1,11 @@
 // Greeting to print in console
-const greeting = "Hello World!";
-const arrayNames = ["Juan", "Antonio", "Marcos"];
+const greeting = 'Hello World!';
+const arrayNames = ['Juan', 'Antonio', 'Marcos'];
 const person = {
   id: 1,
   years: 20,
   printName() {
-    console.log("My name is Antonio");
+    console.log('My name is Antonio');
   }
 };
 
@@ -17,13 +17,13 @@ helloWorld(greeting);
  * @param {string} greeting Greeting to display to user
  */
 function helloWorld(greeting) {
-  console.log("Person", person);
+  console.log('Person', person);
   arrayNames.map(name => console.log(name));
   for (let i = 0; i < arrayNames.length; i++) {
     const name = arrayNames[i];
     console.log(name);
   }
-  if(greeting === "Hi") {
-    console.log("Hi Arkon World!");
-  } else console.log("Say " + greeting);
+  if(greeting === 'Hi') {
+    console.log('Hi Arkon World!');
+  } else console.log('Say ' + greeting);
 }
