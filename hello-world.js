3e023ca109a15bfa702c554092eea348478a60f1
Fide Omar Islas Duarte <fislas@grupoabraxas.com>
2022-06-30 08:36:09 -0500

    Implement no-new-object lint rule for JS

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,7 @@
 // Greeting to print in console
-const greeting = 'Hello World!';
-const arrayNames = ['Juan', 'Antonio', 'Marcos'];
+const greeting = "Hello World!";
+const arrayNames = ["Juan", "Antonio", "Marcos"];
+const person = new Object();
 
 // Function to execute prints
 helloWorld(greeting);
@@ -10,11 +11,12 @@ helloWorld(greeting);
  * @param {string} greeting Greeting to display to user
  */
 function helloWorld(greeting) {
+  console.log("Person", person);
   for (let i = 0; i < arrayNames.length; i++) {
     const name = arrayNames[i];
     console.log(name);
   }
-  if(greeting === 'Hi') {
-    console.log('Hi World!');
-  } else console.log('Say ' + greeting);
+  if(greeting === "Hi") {
+    console.log("Hi World!");
+  } else console.log("Say " + greeting);
 }
